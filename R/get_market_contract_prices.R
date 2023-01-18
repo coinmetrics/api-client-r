bcd50b273ea31e7679f1535c568707e1abc99ca1
Dorian Kandi <dorian.kandi@coinmetrics.io>
2023-01-18 20:54:24 +0000

    contract prices, quotes, implied vols

---
+++ R/get_market_contract_prices.R
@@ -0,0 +1,35 @@
+#' Get Market Contract Prices
+#' @inheritParams get_market_open_interest
+#' @return Tibble of contract prices for specified markets. This includes index price and mark price that are used by the exchange for settlement and risk management purposes.
+#' @export
+get_market_contract_prices <- function(markets,
+                                       start_time = NULL,
+                                       end_time = NULL,
+                                       start_inclusive = TRUE,
+                                       end_inclusive = TRUE,
+                                       timezone = "UTC",
+                                       page_size = NULL,
+                                       paging_from = "end",
+                                       limit_per_market = NULL,
+                                       pretty = FALSE) {
+  query_args <- list(
+    markets = paste0(markets, collapse = ","),
+    start_time = start_time,
+    end_time = end_time,
+    start_inclusive = start_inclusive,
+    end_inclusive = end_inclusive,
+    timezone = timezone,
+    page_size = page_size,
+    paging_from = paging_from,
+    limit_per_market = limit_per_market,
+    pretty = pretty
+  )
+  
+  resp <- send_coinmetrics_request(endpoint = "timeseries/market-contract-prices", query_args = query_args)
+  
+  get_coinmetrics_api_data(
+    api_response = resp,
+    endpoint = "market-contract-prices",
+    paging_from = paging_from
+  )
+}
---
+++ R/get_market_implied_volatility.R
@@ -0,0 +1,34 @@
+#' Get Implied Volatility
+#' @inheritParams get_market_metrics
+#' @return Tibble of implied volatility for specified markets
+#' @export
+get_market_implied_vol <- function(markets,
+                                   start_time = NULL,
+                                   end_time = NULL,
+                                   start_inclusive = TRUE,
+                                   end_inclusive = TRUE,
+                                   timezone = "UTC",
+                                   page_size = NULL,
+                                   paging_from = "end",
+                                   limit_per_market = NULL,
+                                   pretty = pretty) {
+  query_args <- list(
+    markets = paste0(markets, collapse = ","),
+    start_time = start_time,
+    end_time = end_time,
+    start_inclusive = start_inclusive,
+    end_inclusive = end_inclusive,
+    timezone = timezone,
+    page_size = page_size,
+    paging_from = paging_from,
+    limit_per_market = limit_per_market
+  )
+  
+  resp <- send_coinmetrics_request(endpoint = "timeseries/market-implied-volatility", query_args = query_args)
+  
+  get_coinmetrics_api_data(
+    api_response = resp,
+    endpoint = "market-implied-volatility",
+    paging_from = paging_from
+  )
+}
