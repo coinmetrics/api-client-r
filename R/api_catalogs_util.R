34a877c5d14a588d716b1f4e94e225adeff3e995
Dorian Kandi <dorian.kandi@coinmetrics.io>
2023-02-01 03:11:01 +0000

    refactor catalog parsing

--- R/api_catalogs_util.R
+++ R/api_catalogs_util.R
@@ -10,15 +10,82 @@ catalogMarketsData <- function(api_response) {
 }
 
 catalogAssetsData <- function(api_response) {
-  
-  df_assets <- 
-    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>%
-    data.table::setDT()
-  df_assets <- suppressMessages(readr::type_convert(df_assets))
-  
-  tibble::as_tibble(df_assets)
+  RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>% tibble::as_tibble()
 }
 
 catalogMarketMetricsData <- function(api_response) {
   RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>% tibble::as_tibble()
+}
+
+#' @importFrom data.table .SD ":="
+catalogExchangesData <- function(api_response) {
+  
+  df_exchanges <- 
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>% 
+    data.table::setDT()
+
+  df_exchanges[,
+    c('min_time', 'max_time') := lapply(.SD, anytime::anytime),
+    .SDcols = c("min_time", "max_time")
+  ]
+  
+  tibble::as_tibble(df_exchanges)
+}
+
+catalogExchangeAssetsData <- function(api_response) {
+  df_ea <- 
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>% 
+    data.table::setDT()
+  
+  df_ea %>%
+    tidyr::unnest("metrics") %>%
+    tidyr::unnest("frequencies") %>%
+    dplyr::mutate(dplyr::across(c("min_time", "max_time"), anytime::anytime))
+}
+
+catalogPairsData <- function(api_response) {
+  
+  df_pairs <-
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>%
+    data.table::setDT()
+  
+  df_pairs %>%
+    tidyr::unnest("frequencies") %>%
+    dplyr::mutate(dplyr::across(c("min_time", "max_time"), anytime::anytime))
+}
+
+catalogMetricsData <- function(api_response, level) {
+  
+  df_metrics <-
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>%
+    data.table::setDT()
+  
+  df_metrics %>%
+    tidyr::unnest("frequencies") %>%
+    tidyr::unnest({{ level }})
+}
+
+catalogInstData <- function(api_response) {
+  
+  df_inst <-
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>%
+    data.table::setDT()
+  
+  df_inst %>%
+    tidyr::unnest("metrics") %>%
+    tidyr::unnest("frequencies") %>%
+    dplyr::mutate(dplyr::across(c("min_time", "max_time"), anytime::anytime))
+}
+
+catalogMarketCandlesData <- function(api_response) {
+  frequencies = NULL
+  
+  df_candles <-
+    RcppSimdJson::fparse(httr::content(api_response, "raw"), "/data") %>%
+    data.table::setDT(key = "market")
+  
+  df_candles <- df_candles[, unlist(frequencies, recursive = FALSE), by="market"]
+  df_candles[, c("min_time", "max_time") := lapply(.SD, anytime::anytime), .SDcols = c("min_time", "max_time")]
+  
+  tibble::as_tibble(df_candles)
 }
